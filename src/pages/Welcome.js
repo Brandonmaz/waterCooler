e617d49f4148e886bc6026535ff60dd89351533e
banu tryon <banutryon@banus-mbp.lan>
2021-03-06 16:14:22 -0800

    set up login page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import { CredentialsContext } from "../App";
 import Todos from "../components/Todos";
 import "./Welcome.css";
-// import background from "../design/images/watercooler_main.jpg";
+import logo from "../design/logo/logo_nobackgound.svg";
+import background from "../design/images/watercooler_main.jpg";
 export default function Welcome() {
 	const [credentails, setCredentials] = useContext(CredentialsContext);
 	const logout = () => {
@@ -13,20 +14,28 @@ export default function Welcome() {
 
 	return (
 		<div className="body">
+			<img className="glass" src={logo} alt="icon" />
 			<h1 className="welcome">
-				Watercooler {credentails && credentails.username}
+				water cooler {credentails && credentails.username}
 			</h1>
 			<p className="welcomeP">daily good habit reminders</p>
 			<button className="welcomeButton">
 				{!credentails && (
 					<Link className="welcomeRegister" to="/register">
-						Let's get started
+						let's get started
 					</Link>
 				)}
 			</button>
 
 			<br />
-			{!credentails && <Link to="/login">Login</Link>}
+			<div className="signinContainer">
+				<p className="member">Already a member?</p>
+				{!credentails && (
+					<Link className="welcomeLogin" to="/login">
+						Sign in
+					</Link>
+				)}
+			</div>
 			{credentails && <button onClick={logout}>Logout</button>}
 			{credentails && <Todos />}
 		</div>
