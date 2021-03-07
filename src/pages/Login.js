d0c6dc3b1ee74d6be52828e05b474db907735b71
evanepperson <eppersonevan@gmail.com>
2021-03-07 17:46:07 -0600

    saving all css for login and sign up page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,8 @@ import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { CredentialsContext } from "../App";
 import { Link } from "react-router-dom";
+import './Login.css'
+import google from '../design/icons/googlelogo.svg'
 
 export const handleErrors = async (response) => {
 	if (!response.ok) {
@@ -35,7 +37,7 @@ export default function Login() {
 					username,
 					password,
 				});
-				history.push("/");
+				history.push("/todos");
 			})
 			.catch((error) => {
 				setError(error.message);
@@ -45,23 +47,35 @@ export default function Login() {
 	const history = useHistory();
 
 	return (
-		<div>
-			<h1>Login</h1>
-			{error && <span style={{ color: "red" }}>{error}</span>}
-			<form onSubmit={login}>
-				<input
-					onChange={(e) => setUsername(e.target.value)}
-					placeholder="username"
-				/>
-				<br />
-				<input
-					type="password"
-					onChange={(e) => setPassword(e.target.value)}
-					placeholder="password"
-				/>
-				<br />
-				<button type="submit">Login</button>
-			</form>
-		</div>
-	);
+    <div className="logincontainer">
+      <div className="LoginDiv">
+        <h1 className="signinlogin">Sign In</h1>
+        {error && <span style={{ color: "red" }}>{error}</span>}
+        <form onSubmit={login}>
+			<p className="emaillogin">Email</p>
+          <input
+            className="username"
+            onChange={(e) => setUsername(e.target.value)}
+            // placeholder="username"
+          />
+          <br />
+		  <p className="passwordlogin">Password</p>
+          <input
+            className="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            // placeholder="password"
+          />
+          <br />
+		  <p className="forgotpassword">Forgot password?</p>
+          <button className="signinbutton" type="submit">Sign in</button>
+		  <p className='donthave'>Don't have an account? <span className='signinspan'>Sign up</span></p>
+		  <p className="ptagor">Or</p>
+		  <button className="logingoogle">
+			  <img className="googlelogo" src={google} alt="Google Logo" />
+			  <p>Continue with Google</p></button>
+        </form>
+      </div>
+    </div>
+  );
 }
