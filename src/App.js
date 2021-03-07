8286e872516bf3d13155bcec40ca83d7c7fdd523
banu tryon <banutryon@banus-mbp.lan>
2021-03-07 09:54:27 -0800

    styled welcome pagev2

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Register from "./pages/Register";
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
+import Todos from "./components/Todos";
 
 export const CredentialsContext = React.createContext();
 
 function App() {
 	const credentialsState = useState(null);
-	
+
 	return (
 		<div className="App">
 			<CredentialsContext.Provider value={credentialsState}>
@@ -24,6 +25,9 @@ function App() {
 						</Route>
 						<Route exact path="/login">
 							<Login />
+						</Route>
+						<Route exact path="/todos">
+							<Todos />
 						</Route>
 					</Switch>
 				</Router>
