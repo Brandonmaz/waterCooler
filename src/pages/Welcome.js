b7283f09274a3468cc6862d8cdad4fcd78f3db23
banu tryon <banutryon@banus-mbp.lan>
2021-03-07 08:33:20 -0800

    styled welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -7,7 +7,7 @@ import "./Welcome.css";
 import logo from "../design/logo/logo_nobackgound.svg";
 import background from "../design/images/watercooler_main.jpg";
 export default function Welcome() {
-	const [credentails, setCredentials] = useContext(CredentialsContext);
+	const [credentials, setCredentials] = useContext(CredentialsContext);
 	const logout = () => {
 		setCredentials(null);
 	};
@@ -16,11 +16,11 @@ export default function Welcome() {
 		<div className="body">
 			<img className="glass" src={logo} alt="icon" />
 			<h1 className="welcome">
-				water cooler {credentails && credentails.username}
+				water cooler {credentials && credentials.username}
 			</h1>
 			<p className="welcomeP">daily good habit reminders</p>
 			<button className="welcomeButton">
-				{!credentails && (
+				{!credentials && (
 					<Link className="welcomeRegister" to="/register">
 						let's get started
 					</Link>
@@ -30,14 +30,14 @@ export default function Welcome() {
 			<br />
 			<div className="signinContainer">
 				<p className="member">Already a member?</p>
-				{!credentails && (
+				{!credentials && (
 					<Link className="welcomeLogin" to="/login">
 						Sign in
 					</Link>
 				)}
 			</div>
-			{credentails && <button onClick={logout}>Logout</button>}
-			{credentails && <Todos />}
+			{credentials && <button onClick={logout}>Logout</button>}
+			{credentials && <Todos />}
 		</div>
 	);
 }
