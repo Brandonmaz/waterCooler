e2b914d16ec061c8d651098114c1e87c4888c0b4
ajigherighe <akpoigherighe@gmail.com>
2021-03-07 22:41:36 -0500

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ import Login from "./pages/Login";
 import Onboarding1 from "./pages/Onboarding1";
 
 import Todos from "./components/Todos";
-<<<<<<< HEAD
-
-
-=======
 import Account from "./pages/Account";
->>>>>>> ae6433b7d1cfab10ec7c0d0c27f70309409c7793
 export const CredentialsContext = React.createContext();
 
 function App() {
